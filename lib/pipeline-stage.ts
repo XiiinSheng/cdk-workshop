f907f719fddd47e0b2db219fc0c0fd8ec3fe7976
Xin Sheng <xisheng@ucsd.edu>
2024-01-04 14:09:16 -0800

    Add pipeline-stage.ts file

---
+++ lib/pipeline-stage.ts
@@ -0,0 +1,11 @@
+import { CdkWorkshopStack } from './cdk-workshop-stack';
+import { Stage, StageProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class WorkshopPipelineStage extends Stage {
+    constructor(scope: Construct, id: string, props?: StageProps) {
+        super(scope, id, props);
+
+        new CdkWorkshopStack(this, 'WebService');
+    }
+}
